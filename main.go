f4cae85ed5a2a8614f6ffc00cf789aeedf2e4861
Jin <jinhong.chin@afusion.com>
2020-06-05 14:28:30 +0800

    Initial

---
+++ main.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
+
+func Sum(x int, y int) int {
+    return x + y
+}
